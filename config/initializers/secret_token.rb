6711cc8b29e4692a0ae8a16f2c2674ce01ae342b
Roma Sosnovsky <roma.sosnovsky@gmail.com>
2013-03-16 09:23:41 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Damedia::Application.config.secret_token = 'bfbaf27689b5522c7d49f264bc2b81e7a4f044d37b1795d895baa59aeb87e917f956b5be7d1668a1e17baf4daca6dfbfea91c8c6ed823df105a516447ce1caf1'
